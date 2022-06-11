cd4b7fec3497aed0a8176a0b5cda73a07dec4097
Sören Rifé Vinardell <soren@monkingme.com>
2022-06-11 22:18:19 +0200

    Install and setup pre-commit on deps scripts

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -10,6 +10,11 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
+# install pre-commit
+echo "installing pre-commit..."
+brew install pre-commit
+pre-commit install
+
 # install Python requirements
 echo "installing python requirements..."
 pip3 install -r requirements.txt --upgrade --upgrade-strategy eager
