35a9ac543a08dd2dc4bbf797031ca8c5f567483d
Sören Rifé Vinardell <soren@monkingme.com>
2022-11-29 18:30:05 +0100

    Install deps using apt instead of brew

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -4,11 +4,11 @@ set -o pipefail
 
 # install pylint
 echo "installing pylint..."
-brew install pylint
+apt install pylint
 
 # install pre-commit
 echo "installing pre-commit..."
-brew install pre-commit
+apt install pre-commit
 pre-commit install
 
 # install Python requirements
